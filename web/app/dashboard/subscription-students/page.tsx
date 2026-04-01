1a817f8b263b6eec3e8b12056f80c79896415466
ehab <ehabrashedmassoud@gmail.com>
2026-04-01 20:23:24 +0200

    Fix Next page exports for subscription students

--- web/app/dashboard/subscription-students/page.tsx
+++ web/app/dashboard/subscription-students/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { StudentMembersPageContent } from "../student-members/page";
+import { StudentMembersPageContent } from "../student-members/student-members-page-content";
 
 export default function SubscriptionStudentsPage() {
   return <StudentMembersPageContent memberType="group" />;
